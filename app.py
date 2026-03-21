118ea34516aa850f1bc97c83c3c9f1544a756d29
ryo0905 <piaxv95@gmail.com>
2026-03-21 21:46:34 +0900

    flaskのバージョンを固定

--- app.py
+++ app.py
@@ -10,11 +10,14 @@ STATIC_ASSET_VERSION = os.getenv("RENDER_GIT_COMMIT", "dev")
 def create_app() -> Flask:
     app = Flask(__name__, static_folder="static", template_folder="templates", static_url_path="/static")
     app.config["STATIC_ASSET_VERSION"] = STATIC_ASSET_VERSION
+    app.logger.info("Registering routes")
     register_routes(app)
+    app.logger.info("Routes registered")
     return app
 
 
 app = create_app()
+app.logger.info("App instance created")
 
 
 if __name__ == "__main__":
